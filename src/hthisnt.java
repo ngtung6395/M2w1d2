cd683390668f0f24467058583ebb4a039a9e229a
ngtung-scoure <ngtung6395.com>
2021-04-05 09:05:15 +0700

    nop btap

---
+++ src/hthisnt.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class hthisnt {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        int count = 0;
+        int n = 2;
+        for (count < number; count ++){
+            if (isPrime){
+                System.out.println(n);
+                count ++;
+            }
+        }
+    }
+}
---
+++ src/isPrime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class isPrime {
+    public static void main(String[] args) {
+        System.out.println("Enter your number");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        if (num < 2)
+            System.out.println("is NOT a prime");
+        else {
+            boolean check = true;
+            int i = 2;
+            while (i <= Math.sqrt(num)) {
+                if (num % i == 0) {
+                    check = false;
+                }
+                i++;
+            }
+            if (check)
+                System.out.println(num + "is a prime");
+            else
+                System.out.println(num + " is NOT a prime");
+        }
+    }
+}
